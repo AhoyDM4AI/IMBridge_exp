97701683c441a80d606af892e553c548770ac547
lovelynewlife <newlife_1114752@163.com>
2024-09-09 15:20:17 +0800

    feat: add xgboost model training on credit_card dataset.

---
+++ public_datasets/Credit_Card/train_xgboost_gb_credit_card.py
@@ -0,0 +1,52 @@
+import numpy as np
+import pandas as pd
+import pickle
+# from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+# from sklearn.linear_model import LogisticRegression
+import xgboost as xgb
+
+# credit card
+
+#表路径
+path1 = "./creditcard.csv"
+
+#读取csv表
+data = pd.read_csv(path1)
+
+data.dropna(inplace=True)      #删除NaN
+
+#获取分类label
+y = np.array(data.loc[:, 'Class'])
+#28 numerical
+features = []
+for i in range(1, 29):
+        features.append('V{}'.format(i))
+features.append('Amount')
+numerical = np.array(data.loc[:, features])
+print(numerical)
+#Time 计算相隔时间
+#Amount 权重系数
+
+#standard scaling & one-hot encoding
+scaler = StandardScaler()
+standard_scale_model = scaler.fit(numerical)
+
+#获取训练数据
+X = scaler.transform(numerical)
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+print('训练集维度:{}\n测试集维度:{}'.format(X_train.shape, X_test.shape))
+
+# 训练参数
+param = {'objective':'binary:logistic'}
+
+#训练过程
+bst = xgb.XGBClassifier(**param)
+bst.fit(X_train, y_train)
+
+#保存模型
+bst.save_model('./creditcard_xgb_model.json')
+scaler_path = './creditcard_standard_scale_model.pkl'
+with open(scaler_path, 'wb') as f:
+    pickle.dump(standard_scale_model, f)
